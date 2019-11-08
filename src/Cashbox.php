6bb8e1bf8032f379bd5798518e9f62a98f26c198
Anton Yakushev <anton.yakushev@vseinstrumenti.ru>
2019-11-08 18:10:31 +0500

    Проверка push

---
+++ src/Cashbox.php
@@ -0,0 +1,10 @@
+<?php
+
+class Cashbox
+{
+    const STATE_CLOSED = 1;
+    const STATE_OPENED = 2;
+    
+    /** @var integer состояние кассы */
+    private $_state;
+}
---
+++ src/Supermarket.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ${NAMESPACE}\Supermarket.php
+ *
+ * {Добавить описание файла тут}
+ *
+ * @author Якушев Антон <yakushev-ay@yandex.ru>
+ * @copyright ООО Бонусмолл
+ * @package ${NAMESPACE}
+ * Date: 08.11.2019
+ * Time: 10:00
+ */
